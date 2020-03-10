794292bc55a0c892fc6b333d3122a426967cee03
bedamunk <60632339+bedamunk@users.noreply.github.com>
2020-03-10 00:38:02 -0500

    Update Sports.java

--- Sports.java
+++ Sports.java
@@ -5,15 +5,24 @@ final class Sports extends Cars {
 	
 	// constructors
 	public Sports() {
-		
-		
+		super();
 	}
 
 	public Sports(String make, String model, int year, int vinNumber, int raceStats, CarOwners owner) {
 		super (make, model, year, vinNumber, owner);
 		this.raceStats = raceStats;
 	}
-	
+	// equals method
+		public boolean equals (Object obj) {
+			if(!(obj instanceof Sedan)) {
+				return false;
+			}
+			boolean result = false;
+			if(this.getVinNumber() == ((Cars)obj).getVinNumber()) {
+				result = true;
+			}
+			return result;
+		}
 	
 	// setter method to set protected data field raceStats
 			public void setRaceStats(int raceStats) {
@@ -30,7 +39,7 @@ final class Sports extends Cars {
 			
 	@Override
 	public String toString() {
-		String sportscarinfo = super.toString() +  "\nRace Stats: " + raceStats;
+		String sportscarinfo = "This car is a sports car.super.toString()"+  "\nRace Stats: " + this.raceStats;
 		return sportscarinfo;
 	}
 }
