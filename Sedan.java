9df55e9f2b1c4f90804e650b9b62fa23c90a3f2d
bedamunk <60632339+bedamunk@users.noreply.github.com>
2020-03-10 00:37:05 -0500

    Update Sedan.java

--- Sedan.java
+++ Sedan.java
@@ -5,23 +5,32 @@ final class Sedan extends Cars {
 	private int numDoors;
 	private int trunkSize;
 	
+
+	// constructors
 	public Sedan() {
-		
+		super();
 	}
 	
-	// constructors
 	public Sedan(String make, String model, int year, int vinNumber, int numDoors, int trunkSize, CarOwners owner) {
 		super (make, model, year, vinNumber,owner);
 		this.numDoors = numDoors;
 		this.trunkSize = trunkSize;
 	}
-	
+	// equals method
+	public boolean equals (Object obj) {
+		if(!(obj instanceof Sedan)) {
+			return false;
+		}
+		boolean result = false;
+		if(this.getVinNumber() == ((Cars)obj).getVinNumber()) {
+			result = true;
+		}
+		return result;
+	}
 	    // setter method to set protected data field numDoors
 				public void setNumDoors(int numDoors) {
 					this.numDoors = numDoors;
-				}
-				
-				
+				}	
 				
 		// getter method to return private data field numDoors
 				public int getNumDoors() {
@@ -41,7 +50,7 @@ final class Sedan extends Cars {
 				}		
 	@Override
 	public String toString() {
-		String sedancarinfo = super.toString() + "\nNumber of Doors: " + numDoors + "\nTrunk Size: " + trunkSize;
+		String sedancarinfo = "This car is a sedan. " + super.toString() + "\nNumber of Doors: " + this.numDoors + "\nTrunk Size: " + this.trunkSize;
 		return sedancarinfo;
 	}
 }
